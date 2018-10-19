cdbce43bc6ba24bd3857c9b816d06b09c0338920
andrew <amcgladd@gmail.com>
2018-10-19 12:56:54 -0700

    add addEvent function to root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,8 @@ export class AppComponent {
     new Event(19, 11, 2018, "Pioneer Square 701 SW 6th Ave, Portland, OR 97205", 9, 5, 45.518997, -122.679262),
     new Event(20,11, 2018, "950 SW Washington St, Portland, OR 97204", 10, 6, 45.520846, -122.681493)
   ]
+
+  addEvent(newEvent: Event) {
+      this.masterEventList.push(newEvent);
+    }
 }
